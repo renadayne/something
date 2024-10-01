f77a27ebe703d63eb3e78c5aeea46f067bcd0093
renaisme <huytuduelist@gmail.com>
2024-10-01 23:11:55 +0700

    First time create java app

---
+++ relationalmanager/src/main/java/com/fs/pos/relationalmanager/RelationalmanagerApplication.java
@@ -0,0 +1,13 @@
+package com.fs.pos.relationalmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RelationalmanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RelationalmanagerApplication.class, args);
+	}
+
+}
---
+++ relationalmanager/src/test/java/com/fs/pos/relationalmanager/RelationalmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fs.pos.relationalmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RelationalmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
